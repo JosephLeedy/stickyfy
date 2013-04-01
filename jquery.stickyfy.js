7ab0e6b6cb23347b082d1ac168023d3eb72286a2
Joseph Leedy <joseph@paradoxlabs.com>
2013-04-01 14:28:04 -0400

    fixed headers

--- jquery.stickyfy.js
+++ jquery.stickyfy.js
@@ -10,7 +10,7 @@
  * @created 2013-03-29
  * @updated 2013-04-01
  *
- * @license Copyright (c) 2013 ParadoxLabs, inc.
+ * Copyright (c) 2013 ParadoxLabs, inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -26,7 +26,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA.
  */
-/* @preserve jQuery Stickyfy Plug-in <https://github.com/jmleedy/stickyfy> Copyright (c) 2013 ParadoxLabs, inc. */
 (function($) {
 	$.fn.stickyfy = function(options) {
 		options = options || {};
